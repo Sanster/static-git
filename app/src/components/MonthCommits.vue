d952cbf94777819fe58a493f721d5dc503e23c69
Sanster <cwq1913@163.com>
2016-10-27 13:32:19 +0800

    display select month data

--- app/src/components/MonthCommits.vue
+++ app/src/components/MonthCommits.vue
@@ -1,5 +1,17 @@
 <template>
-<canvas id='month-commits' width='500' height='350'></canvas>
+<div>
+  <canvas id='month-commits' width='500' height='350'></canvas>
+  <div>
+    {{monthData[selectedMonth]}} Commits this month.
+    <h3>Top 5</h3>
+    <ul>
+      <li v-for="n in 5">
+        {{barChartData[n].name}}
+        {{authorMonthCommitsCount(n)}} commits this month.
+      </li>
+    </ul>
+  </div>
+</div>
 </template>
 
 <script>
@@ -11,28 +23,38 @@ export default {
   ],
   data () {
     return {
-      downSort: true
+      monthData: [],
+      selectedYear: '2016',
+      selectedMonth: 0
     }
   },
   methods: {
     barClick (event, data) {
-      console.log(event)
-      console.log(data[0]._index)
+      if (data.length !== 0) {
+        this.selectedMonth = data[0]._index
+        this.barChartData.sort(this.sortByMonthCommits('2016', this.selectedMonth))
+      }
     },
-    monthData () {
+    calMonthData () {
       const data = {}
       const year = '2016'
       data[year] = new Array(12).fill(0)
-      console.log(data)
+
       for (var i = 0, l = this.barChartData.length; i < l; ++i) {
         for (var j = 0; j < 12; ++j) {
           data[year][j] += this.barChartData[i].commitsCountByDay[year][j].count
         }
       }
-      return data[year]
+      this.monthData = data[year]
+    },
+    sortByMonthCommits () {
+    },
+    authorMonthCommitsCount (index) {
+      return this.barChartData[index].commitsCountByDay[this.selectedYear][this.selectedMonth].count
     }
   },
   mounted () {
+    this.calMonthData()
     var ctx = document.getElementById('month-commits')
 
     new Chart(ctx, {
@@ -41,7 +63,7 @@ export default {
         labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
         datasets: [{
           label: 'Commits',
-          data: this.monthData(),
+          data: this.monthData,
           backgroundColor: 'rgba(75, 192, 192, 0.8)'
           // borderColor: 'rgba(75, 192, 192, 0.2)',
         }]
